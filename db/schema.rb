bceb0115508014262606731d662f19749fbc63e4
Solomon Glover <solomon.glover@hgs.hiddengeniusproject.org>
2020-02-08 12:14:20 -0800

    Styling and Structure

--- db/schema.rb
+++ db/schema.rb
@@ -18,5 +18,4 @@ ActiveRecord::Schema.define(version: 2020_01_16_013603) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
